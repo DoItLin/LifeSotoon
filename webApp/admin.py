35c42362317ffd72ef2b0c06f99a67e41544307f
LinUX-MI <lin@Lin>
2024-05-09 16:59:12 +0330

    add in_comes to models.

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from .models import   out_comes
-
+from .models import in_comes
 # Register your models here.
 
 admin.site.register(out_comes)
-
+admin.site.register(in_comes)
--- webApp/models.py
+++ webApp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+
+#How much that i spent money today and for what? 
 class out_comes(models.Model):
 
     text=models.CharField(max_length=255)
@@ -9,3 +11,10 @@ class out_comes(models.Model):
     amount=models.BigIntegerField()
     user=models.ForeignKey(User ,on_delete=models.CASCADE)
 
+
+#How much that i get money today and how I reach it ? 
+class in_comes(models.Model):
+    text=models.CharField( max_length=250)
+    date=models.DateTimeField()
+    amount=models.BigIntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE) 
