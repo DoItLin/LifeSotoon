a55ebb0ccb6d98204b6cb8aebfb1b9f5f6a1a9bf
Lin <102280598+DoItLin@users.noreply.github.com>
2024-05-14 01:24:26 +0330

    Update admin.py

--- webApp/admin.py
+++ webApp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-
 from .models import   out_comes
 from .models import in_comes
+from .models import Token 
+
 # Register your models here.
 
 admin.site.register(out_comes)
 admin.site.register(in_comes)
+admin.site.register(Token)
+
